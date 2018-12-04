3cbb5e409ed946a7a4d5d0f72f255fccf0d816a8
Corazon <38621418+LeeeRoux@users.noreply.github.com>
2018-12-04 11:46:00 -0800

    v1.3.7

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-Array.prototype.random = function() {
-return this[Math.floor(Math.random() * this.length)]}
-
  const letters = require("./bin/letters.js")
  , toLatin = require("./bin/toLatin.js")
  , compare = require("./bin/compare")
  , keyGenerator = require("./bin/keyGenerator.js")
+ , calculateLatin = require("./bin/calculateLatin.js")
+ , between = require("./bin/between.js");
 
 const chars = {
  letters,
  toLatin,
  compare,
  keyGenerator,
- btxt: function (txt) {
-  if (typeof txt !== 'string') throw new TypeError(`You must provide a String. @"${typeof txt}"`)
+ calculateLatin,
+ between,
+btxt: function (txt) {
+ if (typeof txt !== 'string') throw new TypeError(`Argument "txt" must be a String.`)
   var finnalWord = '';
   for (var i = 0; i < txt.length + 1; i++) {
-    if (this.letters.list.includes(txt.charAt(i).toLowerCase())) {
-     finnalWord += this.letters[`char${txt.charAt(i).toUpperCase()}`][txt.charAt(i)].random();
-    } else {
-     finnalWord += txt.charAt(i);
-   }
-  }
- return finnalWord;
- }
+if (this.letters.list.includes(txt.charAt(i).toLowerCase())) {
+ finnalWord += this.letters[`char${txt.charAt(i).toUpperCase()}`][txt.charAt(i)].random();
+  } else {
+ finnalWord += txt.charAt(i);
+   }}
+ return finnalWord;}
 };
+
+Array.prototype.random = function() {
+return this[Math.floor(Math.random() * this.length)]}
 
 module.exports = chars;
